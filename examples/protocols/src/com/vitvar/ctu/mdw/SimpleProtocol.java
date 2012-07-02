0730dc43cef740e74ff488c798abd9fabaea6a51
Tomas Vitvar <tomas@vitvar.com>
2012-07-02 15:08:51 +0200

    simple protocol example

--- examples/protocols/src/com/vitvar/ctu/mdw/SimpleProtocol.java
+++ examples/protocols/src/com/vitvar/ctu/mdw/SimpleProtocol.java
@@ -5,10 +5,10 @@ import java.net.*;
 import java.util.regex.*;
 
 /**
- * Simple protocol example. The class starts a server that listens on the port 8080,
- * and parses the input in a form add a b, where a and b are integer values, adds the two
- * numbers and sends the result back to the client. The communication ends when the client
- * sends bye command.
+ * Simple protocol example. The class starts a listener on the port 8080.
+ * When a client connects, the server parses the input in a form "add a b", 
+ * where "a" and "b" are integer values, adds the two numbers and sends 
+ * the result back to the client. The communication ends when the client sends "bye".
  * 
  * @author tomas@vitvar.com
  *
@@ -25,7 +25,8 @@ public class SimpleProtocol {
 		
 		// create reader and writer to read from and write to the socket
 		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		BufferedReader in = new BufferedReader(
+			new InputStreamReader(clientSocket.getInputStream()));
 		
 		// print information to the client
 		out.println("verbs: add a b, bye");
@@ -37,7 +38,8 @@ public class SimpleProtocol {
 		// read input from the client and process the input
 		while ((message = in.readLine()) != null) {   
 			if ((m = p.matcher(message)).matches())
-				out.println("Result: " + (Integer.parseInt(m.group(1)) + Integer.parseInt(m.group(2))));
+				out.println("Result: " + (Integer.parseInt(m.group(1)) + 
+					Integer.parseInt(m.group(2))));
 			else
 				if (message.equals("bye")) {
 					out.println("Goodbye!");
