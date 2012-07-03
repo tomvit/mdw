794f4a1adc3e8f04681d10da07207577746e05c0
Tomas Vitvar <tomas@vitvar.com>
2012-07-03 11:58:00 +0200

    counter example

--- examples/protocols/src/com/vitvar/ctu/mdw/Sessions.java
+++ examples/protocols/src/com/vitvar/ctu/mdw/Sessions.java
@@ -10,17 +10,14 @@ import javax.servlet.http.HttpServletRequest;
 /** Class for the session management that uses HTTP cookies **/
 public class Sessions<E> {
 	
-	// storage for the session data; key is a session id, value is a string
-	// that represents the session data
+	// storage for the session data;
 	private Hashtable<String, E> sessions = new Hashtable<String, E>();
 	
 	/** Returns session id based on the information in the http request **/
 	public String getSessionID(HttpServletRequest request) throws Exception {
-		// session id
 		String sid = null;
 
-		// test if the client sent the session information
-		// and extract the session id from the cookie
+		// extract the session id from the cookie
 		if (request.getHeader("cookie") != null) {
 			Pattern p = Pattern.compile(".*session-id=([a-zA-Z0-9]+).*");
 			Matcher m = p.matcher(request.getHeader("cookie"));
@@ -30,15 +27,13 @@ public class Sessions<E> {
 		
 		// create a new session id if none exists 
 		if (sid == null || sessions.get(sid) == null) {
-			// create the session id md5 hash; maximum of 1000 connections 
-            // from a single IP
+			// create the session id md5 hash; max 1000 connections per IP 
 			MessageDigest md = MessageDigest.getInstance("MD5");
 			md.update(new String(request.getRemoteAddr() + 
 				Math.floor(Math.random()*999)).getBytes());
 			sid = Utils.toHexString(md.digest());
 		}
 		
-		// return the session id
 		return sid;
 	}
 	
