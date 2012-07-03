293ccbd3bc0cf58ce83594933b5dbfd527e68271
Tomas Vitvar <tomas@vitvar.com>
2012-07-03 12:09:11 +0200

    counter example

--- examples/protocols/src/com/vitvar/ctu/mdw/Counter.java
+++ examples/protocols/src/com/vitvar/ctu/mdw/Counter.java
@@ -14,16 +14,17 @@ public class Counter extends AbstractHttpListener {
 	@Override
 	public void handleRequest(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		
+		// get the session id
 		String sid = sessions.getSessionID(request);
 		
-		System.out.println(sid);
-				
+		// create the new data if none exists
 		if (sessions.getData(sid) != null)
-			sessions.setData(sid, Integer.valueOf(sessions.getData(sid).intValue() + 1));
+			sessions.setData(sid, 
+				Integer.valueOf(sessions.getData(sid).intValue() + 1));
 		else
 			sessions.setData(sid, Integer.valueOf(1));
 		
+		// send the response
 		response.setStatus(200);
 		response.setHeader("Set-Cookie", "session-id="+ sid + "; MaxAge=3600");
 		response.setHeader("Content-Type", "text/plain");
