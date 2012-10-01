218d905697301a5c1a63706d97d6e311e4009b1c
Tomas Vitvar <tomas@vitvar.com>
2012-10-01 21:06:10 +0200

    examples

---
+++ examples/scripts/compile.sh
@@ -0,0 +1,19 @@
+d=$pwd
+cd $(dirname $0)
+
+mkdir -p ../bin
+
+CP="../libs/jetty-all-7.0.2.v20100331.jar:../libs/servlet-api-2.5.jar:../protocols/src"
+javac -cp $CP -d ../bin ../protocols/src/com/vitvar/ctu/mdw/SimpleProtocol.java
+javac -cp $CP -d ../bin ../protocols/src/com/vitvar/ctu/mdw/Sessions.java
+javac -cp $CP -d ../bin ../protocols/src/com/vitvar/ctu/mdw/Utils.java
+javac -cp $CP -d ../bin ../protocols/src/com/vitvar/ctu/mdw/HttpListener.java
+javac -cp $CP -d ../bin ../protocols/src/com/vitvar/ctu/mdw/Counter.java
+javac -cp $CP -d ../bin ../protocols/src/com/vitvar/ctu/mdw/AbstractHttpListener.java
+
+CP="../RMITest/src"
+javac -cp $CP -d ../bin ../RMITest/src/com/vitvar/ctu/mdw/RMIClient.java
+javac -cp $CP -d ../bin ../RMITest/src/com/vitvar/ctu/mdw/RMIServer.java
+javac -cp $CP -d ../bin ../RMITest/src/com/vitvar/ctu/mdw/HelloRMIInterface.java
+
+cd $d
