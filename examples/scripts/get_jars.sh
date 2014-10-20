e5b5ba8cbbd0cb85d43269f5abcb9f0cebd44066
Tomas Vitvar <tomas@vitvar.com>
2014-10-20 21:55:12 +0200

    examples

--- examples/scripts/get_jars.sh
+++ examples/scripts/get_jars.sh
@@ -5,13 +5,17 @@ nd=$(dirname $0)/../libs
 mkdir -p $nd
 cd $nd
 
-echo "Downloading jetty v7.0.2..."
+echo "* Downloading jetty v7.0.2..."
 rm -f jetty-all-7.0.2.v20100331.jar
 wget -q http://dist.codehaus.org/jetty/jetty-hightide-7.0.2/jetty-all-7.0.2.v20100331.jar
 
-echo "Downloading servlet API v2.5..."
+echo "* Downloading servlet API v2.5..."
 rm -f servlet-api-2.5.jar
 wget -q http://mirrors.ibiblio.org/pub/mirrors/maven2/javax/servlet/servlet-api/2.5/servlet-api-2.5.jar
 
-echo "Done."
+echo "* Downloading jms API v1.1"
+rm -f jms-1.1jar
+wget -q https://repository.jboss.org/nexus/content/groups/public/javax/jms/jms/1.1/jms-1.1.jar
+
+echo "Done!"
 cd $od
